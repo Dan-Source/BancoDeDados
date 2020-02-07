b69da1ec7a419c19be103f42af8024c3b41a574e
Daniel Luis <52837045+Dan-Source@users.noreply.github.com>
2020-02-06 21:31:45 -0300

    Update Criando_Banco_E_Adicionando_Dados.sql

--- Criando_Banco_E_Adicionando_Dados.sql
+++ Criando_Banco_E_Adicionando_Dados.sql
@@ -29,3 +29,27 @@ VALUES ('Ariquemes', '15', 'F');
 
 /* Apresenta uma descrição da tabela*/
 desc Alunos;
+
+/* Criando uma tabela com dados da disciplina*/
+CREATE TABLE Disciplinas( 
+	nome varchar(40),
+    descricao varchar(200),
+    modulo char(3)
+);
+
+/* Inserindo dados para disciplina*/
+INSERT INTO Disciplinas (nome, descricao, modulo)
+VALUES ('Banco de Dados', 'Ensinar como se manipulas, cria e gerencia Banco de Dados', 'iii');
+
+INSERT INTO Disciplinas (nome, descricao, modulo)
+VALUES ('Programação Para Web', 'Ensinar a desenvolver sites', 'iii');
+
+INSERT INTO Disciplinas (nome, descricao, modulo)
+VALUES ('Arquitetura de Computadores', 'Ensinar como funciona uma maquina computacional', 'iii');
+
+INSERT INTO Disciplinas (nome, descricao, modulo)
+VALUES ('Sistemas Operacionais', 'Aprender os principios de sistemas operacionais na computação', 'iii');
+
+INSERT INTO Disciplina (nome, descricao, modulo)
+VALUES ('projeto Integrador', 'Incentivar os alunos a utilizarem os conhecimentos ao projetar um sitema', 'iii');
+
