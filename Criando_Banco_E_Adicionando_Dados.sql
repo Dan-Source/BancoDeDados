8ed115d7758defb9917c4ec92fe526c27ae121a2
Daniel Luis <52837045+Dan-Source@users.noreply.github.com>
2020-02-12 21:27:29 -0300

    Projecao, seleção e junção

--- Criando_Banco_E_Adicionando_Dados.sql
+++ Criando_Banco_E_Adicionando_Dados.sql
@@ -53,3 +53,40 @@ VALUES ('Sistemas Operacionais', 'Aprender os principios de sistemas operacionai
 INSERT INTO Disciplina (nome, descricao, modulo)
 VALUES ('projeto Integrador', 'Incentivar os alunos a utilizarem os conhecimentos ao projetar um sitema', 'iii');
 
+/*Projecao, Selecao e Juncao*/
+
+/* projetar informações na tela*/
+SELECT nome as Nome, idade from Alunos;
+
+/* Selecao de informações de um banco de Dados*/
+
+SELECT nome, idade from Alunos
+WHERE sexo = 'M';
+
+/* Mostrar hora atual*/
+
+SELECT now();
+
+/* Mostrar Nome na Tela*/
+
+SELECT 'AESSHEDAI';
+
+/* Mostrar NOME como ALUNO*/
+
+SELECT nome as alunos FROM Alunos;
+
+/* Projecao dos Nomes de sexo Feminino*/
+
+SELECT nome, sexo FROM Alunos
+WHERE sexo = 'F';
+
+/* Selecao de dados por modulo*/
+
+SELECT nome, modulo FROM Disciplinas
+WHERE modulo ='ii';
+
+/* Projecao de nomes com idade maior que 15 anos e do sexo 'F'*/
+
+SELECT nome, sexo FROM Alunos
+WHERE sexo = 'F' and idade > 15;
+
