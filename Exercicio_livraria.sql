34a11ef1b4b7f3719ec66645f9c1b984e49a0f01
Daniel Luis <52837045+Dan-Source@users.noreply.github.com>
2020-02-18 19:21:27 -0300

    Correção do Professor

--- Exercicio_livraria.sql
+++ Exercicio_livraria.sql
@@ -2,6 +2,9 @@
 
 CREATE DATABASE Livraria;
 
+/* Mostrar todos os BDs*/
+SHOW DATABASES;
+
 /* Usando Banco de Dados*/
 
 USE Livraria;
@@ -18,17 +21,23 @@ CREATE TABLE Livros (
     uf_editora CHAR(2),
     ano_publicacao INT(4)
 );
+-- Não utilizar o tamanho para variaveis númericas
 
+-- Para apagar a tabela, utilizamos o seguinte comando:
 DROP TABLE Livros;
+
 /*1*/
 INSERT INTO Livros VALUES ('Cavaleiro Real', 'Ana Claudia', 'Feminino', 465, 'Atlas', 49.9, 'RJ', 2019);
+
 /*2*/
 INSERT INTO Livros VALUES ('SQL para Leigos', 'João Nunes', 'Masculino', 450, 'Adison', 98.0, 'SP', 2018);
 /*3*/
 
 INSERT INTO Livros VALUES ('Receitas Caseiras', 'Cella Tavares', 'Feminino', 210 , 'Atlas', 45, 'RJ', 2008);
+
 /*4*/
 INSERT INTO Livros VALUES ('Pessoas Efetivas', 'Eduardo Santos', 'Masculino', 210 , 'Beta', 78.99, 'RJ', 2018);
+
 /*5*/
 INSERT INTO Livros VALUES ('Habitos Saudáveis', 'Eduardo Santos', 'Masculino', 630, 'Beta', 150.98 , 'RJ', 2019);      
 
@@ -49,28 +58,55 @@ INSERT INTO Livros VALUES ('O poder da mente', 'Clara Mafra ', 'Feminino', 120 ,
 
 -- Queries
 
--- Trazer todos os dados
-
-SELECT * FROM Livros;
+SELECT 
+    *
+FROM
+    Livros;
 
 -- Trazer Nome do Livro e da Editora
 
-SELECT nome_livro, nome_editora FROM Livros;
+SELECT 
+    nome_livro, nome_editora
+FROM
+    Livros;
 
 -- Trazer Nome do Livro, com UF de Autores Masculinos
 
-SELECT nome_livro, uf_editora FROM Livros WHERE sexo_autor = 'Masculino';
+SELECT 
+    nome_livro, uf_editora
+FROM
+    Livros
+WHERE
+    sexo_autor = 'Masculino';
 
 -- Trazer Nome do Livro, com numero de paginas  de Autores Femininos
 
-SELECT nome_livro, numero_paginas FROM Livros WHERE sexo_autor = 'Feminino';
+SELECT 
+    nome_livro, numero_paginas
+FROM
+    Livros
+WHERE
+    sexo_autor = 'Feminino';
 
 -- Trazer todos os dados da editora de SP
 
-SELECT * FROM Livros WHERE uf_editora='SP';
+SELECT 
+    *
+FROM
+    Livros
+WHERE
+    uf_editora = 'SP';
 
 
 -- Trazer todos os dados dos Autores do Sexo Masculino Com livros publicados em SP ou RJ
 
-SELECT * FROM Livros WHERE (uf_editora='SP' OR uf_editora='RJ') AND sexo_autor = 'Masculino';
+SELECT 
+    *
+FROM
+    Livros
+WHERE
+    (uf_editora = 'SP' OR uf_editora = 'RJ')
+        AND sexo_autor = 'Masculino';
+
+
 
